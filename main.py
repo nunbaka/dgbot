197804091750e60dad680197da4c55489f771fdd
PandawanShow <cjosepppp@gmail.com>
2020-06-21 23:29:40 -0300

    client and conventions

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#User = Discord
+import discord
+
+#user = UserDiscord
+#users = Lista de UserDiscord
+#author = UserDicord Que Enviou a Mensagem
+
+#player = Nosso tipo de dado que representa um player
+#players = Lista de players
+#target = Player que vai sofrer a ação
+
+#guild = Guild do discord
+#Club = Nosso tipo de dado que representa a guild
+
+class Client(discord.Client):
+    def __init__(self):
+        pass
+    async def run(self):
+        pass
+    async def on_message(self, message):
+        pass
+    async def on_ready(self):
+        pass
+client = Client()
