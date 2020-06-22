896bedc40948b120a40bd05f11d132370e3c3703
PandawanShow <cjosepppp@gmail.com>
2020-06-22 02:04:53 -0300

    Basic Structure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
-from library import Json, getCurrentTime
+from library import Json, getCurrentTime, existKey
+from context import Context
+from classes.Club import Club
 
 #user = UserDiscord
 #users = Lista de UserDiscord
@@ -11,28 +13,63 @@ from library import Json, getCurrentTime
 
 #guild = Guild do discord
 #club = Nosso tipo de dado que representa a guild
+#clubs, uma lista de clubs
 
 class Client(discord.Client):
     def __init__(self):
         super().__init__()
+        #self.tokens contém os tokens de todos os bots
         self.tokens = Json.loadWrite(pathfile='private/token.json')
+        #self.bot contém qual bot está sendo utilizado
         self.bot = "dev"
         self.version = "0.001"
         self.name = "DGBot"
-        self.guildManagers = {}
+        self.clubs = {}
+        #self.prefixes contém os prefixos de cada club 
         self.prefixes = Json.loadWrite(pathfile='private/prefixes.json')
+        #roda o client pela chave do token
         self.run(self.tokens[self.bot])
 
     async def on_ready(self):
+        #pega o tempo atual
         cur_time = getCurrentTime()
+        #envia informações da inicialização
         print(f"{self.name} [{self.bot}] - {self.version} init at {cur_time}")
+        #informações sobre cada guild
         for guild in self.guilds:
             print(f"\t{guild.name}:{guild.id} connected")
 
     async def on_message(self, message):
         if message.author == self.user:
+            #verifica se a mensagem é do proprio bot
             return
         if not message.guild:
+            #verifica se a mensagem foi enviada em uma guild
             return
+        #recebe o prefixo da guild em questão
+        prefix = self.getPrefix(message.guild)
+        #instancia um club a partir da guild
+        club = self.getClub(message.guild)
+        #instancia um objeto contendo as informações do contexto
+        context = Context(self, prefix, message)
+        await club.run(context)
+    
+    def getClub(self, guild):
+        #cria a chave da guild
+        cKey = str(guild.id)
+        if not existKey(cKey, self.clubs):
+            #se a chave não existir no dicionario, instancia um club na referencia
+            self.clubs[cKey] = Club(cKey)
+        return self.clubs[cKey]
+
+    def getPrefix(self, guild):
+        #cria a chave da guild
+        cKey = str(guild.id)
+        if not existKey(cKey, self.prefixes):
+            #se o prefixo não existir
+            return '/'
+        else:
+            #se existir prefixo, retorne-o
+            return self.prefixes[cKey]
 
 client = Client()
